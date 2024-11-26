04e3ed75cb7daf75c19ee870101e3e8cb0999c2f
Sato3605 <rafinhasato7@gmail.com>
2024-11-26 15:42:29 -0300

    prova pronta

---
+++ Rafael/Rafael/Models/Funcionario.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Rafael.Models;
+
+public class Funcionario
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+    public string CPF { get; set; }
+}
